02b9f081078930fab4b5309a7fed51a94fbe7ae8
RJ Singh <67853007+rjsiinghhh@users.noreply.github.com>
2022-08-31 12:27:28 -0700

    Add project to repo

---
+++ views/home.js
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native';
+
+
+export default function Home() {
+    return (
+      <View>
+        <Text>
+            Hello, HOME PAGEE
+        </Text>
+      </View>
+    );
+  }
+  
